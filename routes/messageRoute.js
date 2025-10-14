76b127909f12fc57905465eea916d4b766f530b0
Kazim Amiri <mohammad.kazimamiri2001@gmail.com>
2025-10-14 21:44:59 +0430

    backend setups has been completed

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -4,6 +4,7 @@ import {
   getMessages,
   getUsersForSidebar,
   markMessageAsSeen,
+  sendMessage,
 } from "../controllers/messageController";
 
 const messageRouter = express.Router();
@@ -11,3 +12,6 @@ const messageRouter = express.Router();
 messageRouter.get("/users", protectedRoute, getUsersForSidebar);
 messageRouter.get("/:id", protectedRoute, getMessages);
 messageRouter.put("/mark/:id", protectedRoute, markMessageAsSeen);
+messageRouter.post("/send/:id", protectedRoute, sendMessage);
+
+export default messageRouter;
--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-import mongoose from "mongoose";
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoute.js";
+import { server } from "socket.io";
 
 // create express app and HTTP server
 const app = express();
 const server = http.createServer(app);
+
+//intialize socket.io server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+// store online users
+export const userSocketMap = {};
+
+// Socket.io connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("user connected", userId);
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //emit online users to connected users
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnected", () => {
+    console.log("user disconnected", userId);
+    delete userSocketMap(userId);
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
@@ -17,6 +43,7 @@ app.use(cors());
 // Test route
 app.use("/api/status", (req, res) => res.send("server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 // MongoDB connection
 await connectDB();
